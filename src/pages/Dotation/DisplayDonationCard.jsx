5ddaf964c8eaf2ef4e43f2e83d8813910b3523bf
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-09-26 10:11:33 +0600

    Donation page done with Seee All button function

---
+++ src/pages/Dotation/DisplayDonationCard.jsx
@@ -0,0 +1,39 @@
+
+
+const DisplayDonationCard = ({ donation }) => {
+    const { id, picture, category, title, price, card_bg_color, category_bg_color, text_button_bg_color } = donation;
+
+    const cardStyle = {
+        backgroundColor: card_bg_color,
+    };
+
+    const textColor = {
+        color: text_button_bg_color
+    }
+    const bgColor = {
+        backgroundColor: text_button_bg_color
+    }
+
+    return (
+        <div>
+            <div className="card card-side bg-base-100 shadow-xl rounded-lg" style={cardStyle}>
+                <img className="rounded-s-lg" src={picture} alt="donation" />
+                <div className="card-body">
+                    <p className="w-fit text-sm font-medium text-center leading-snug tracking-normal px-[10px] py-1 rounded " style={{
+                        backgroundColor: category_bg_color,
+                        color: text_button_bg_color,
+                    }}>
+                        {category}
+                    </p>
+                    <h2 className="text-xl font-semibold">{title} </h2>
+                    <p className="text-base font-semibold" style={textColor}> ${price} </p>
+                    <div className="mt-1">
+                        <button className="text-xl font-semibold text-white px-3 py-1 rounded" style={bgColor}>View Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DisplayDonationCard;
--- src/pages/Dotation/Dotation.jsx
+++ src/pages/Dotation/Dotation.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import DisplayDonationCard from "./DisplayDonationCard";
 
 const Dotation = () => {
+
+    const [donations, setDonations] = useState([]);
+    const [showAll, setShowAll] = useState(false);
+
+    // load data from local storage
+    useEffect(() => {
+        const donatedItems = JSON.parse(localStorage.getItem('donation-item'));
+        if (donatedItems) {
+            setDonations(donatedItems);
+        }
+    }, [])
+
+    console.log(donations);
+    const visibleDonation = showAll ? donations : donations.slice(0, 4);
+
     return (
-        <div>
-            this is dotation
+        <div className="my-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {
+                    visibleDonation.map(donation => (<DisplayDonationCard key={donation.id} donation={donation}></DisplayDonationCard>))
+                }
+            </div>
+            <div className="mt-10">
+                {donations.length > 4 && !showAll && <button onClick={() => setShowAll(true)} className="block mx-auto bg-[#009444] rounded-lg text-white p-2">See All</button>
+                }
+            </div>
         </div>
     );
 };
