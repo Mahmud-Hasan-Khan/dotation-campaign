be861c438ec296498f4fc439e8642f274581b48d
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-09-26 20:33:52 +0600

    404 page created

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>Oops!!</h1>
-            <Link to='/'>Back to Home </Link>
+        <div className='flex flex-col items-center justify-center my-6 gap-3'>
+            <img className='object-center' src="https://i.ibb.co/k0KXXrR/404-page-cover.jpg" alt="" />
+            <Link to='/'> <button className='bg-red-500 text-white p-1 rounded-md'>Back to Home</button></Link>
         </div>
     );
 };
